ef855158fbc3725db6168d3cb11114a2bba1d402
canasmh <canasmh@yahoo.com>
2022-07-26 13:13:15 -0400

    Add styling to the celebrity banner

--- client/src/Celeb.js
+++ client/src/Celeb.js
@@ -2,20 +2,21 @@ import React from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
+import "./Celeb.css"
 
 function Celeb() {
 
     return (
-        <div>
-            <h1>Celebrity Name</h1>
-            <p>Occupation: First Occ, Second Occ, Third Occ</p>
+        <div className="celeb-container">
+            <h1 >Manuel Cañas</h1>
+            <p>Occupation: Developer, Astrophysicist</p>
             <Container>
                 <Row>
                     <Col xsm={12}><p>Google Search: <a href="http://google.com/search?q=Dwayne+Johnson" target="_blank" rel="noreferrer">Manuel Canas</a></p></Col>
                 </Row>
                 <Row>
                     <Col xsm={6}><p>Birthday: June 26, 1996</p></Col>
-                    <Col xsm={6}><p>Country of Origin: Colombia</p></Col>
+                    <Col xsm={6}><p>Nationality: Colombia</p></Col>
                 </Row>
             </Container>
         </div>
