af1f776de47c5d9e557cd69fee09f3c1750629a0
canasmh <canasmh@yahoo.com>
2022-07-26 13:42:42 -0400

    Create the game-board container

---
+++ client/src/GameBoard.js
@@ -0,0 +1,19 @@
+import "./GameBoard.css"
+
+function GameBoard() {
+    return (
+        <div className="game-board">
+            <p>One Row of Tiles</p>
+            <p>One Row of Tiles</p>
+            <p>One Row of Tiles</p>
+            <p>One Row of Tiles</p>
+            <p>One Row of Tiles</p>
+            <p>One Row of Tiles</p>
+            <p>One Row of Tiles</p>
+            <p>One Row of Tiles</p>
+            <p>One Row of Tiles</p>
+        </div>
+    )
+}
+
+export default GameBoard;
