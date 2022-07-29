0d73ee0203f11068195b5fda1aa3a36b63a6709c
canasmh <canasmh@yahoo.com>
2022-07-29 18:11:21 -0400

    Set the props that will be used to update the game tiles

--- client/src/GameBoard.js
+++ client/src/GameBoard.js
@@ -1,14 +1,17 @@
 import GameTiles from "./GameTiles";
 import "./GameBoard.css"
 
-function GameBoard() {
+function GameBoard(props) {
+
+    const guesses = props.guesses;
+
     return (
         <div className="game-board">
-            <GameTiles />
-            <GameTiles />
-            <GameTiles />
-            <GameTiles />
-            <GameTiles />
+            <GameTiles guesses={guesses.length > 1 ? guesses[0] : null} />
+            <GameTiles guesses={guesses.length > 2 ? guesses[1] : null} />
+            <GameTiles guesses={guesses.length > 3 ? guesses[2] : null} />
+            <GameTiles guesses={guesses.length > 4 ? guesses[3] : null} />
+            <GameTiles guesses={guesses.length > 5 ? guesses[4] : null} />
         </div>
     )
 }
