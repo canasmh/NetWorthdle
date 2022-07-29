757cf510b6534fccaece1cfeff689495fe90a34f
canasmh <canasmh@yahoo.com>
2022-07-29 18:17:52 -0400

    Update form inputs according to guess

--- client/src/GameBoard.js
+++ client/src/GameBoard.js
@@ -7,11 +7,11 @@ function GameBoard(props) {
 
     return (
         <div className="game-board">
-            <GameTiles guesses={guesses.length > 1 ? guesses[0] : null} />
-            <GameTiles guesses={guesses.length > 2 ? guesses[1] : null} />
-            <GameTiles guesses={guesses.length > 3 ? guesses[2] : null} />
-            <GameTiles guesses={guesses.length > 4 ? guesses[3] : null} />
-            <GameTiles guesses={guesses.length > 5 ? guesses[4] : null} />
+            <GameTiles guess={guesses.length > 1 ? guesses[0] : null} />
+            <GameTiles guess={guesses.length > 2 ? guesses[1] : null} />
+            <GameTiles guess={guesses.length > 3 ? guesses[2] : null} />
+            <GameTiles guess={guesses.length > 4 ? guesses[3] : null} />
+            <GameTiles guess={guesses.length > 5 ? guesses[4] : null} />
         </div>
     )
 }
--- client/src/GameTiles.js
+++ client/src/GameTiles.js
@@ -3,12 +3,15 @@ import Form from "react-bootstrap/Form";
 import "./GameTiles.css"
 
 
-function GameTiles() {
+function GameTiles(props) {
+
+        const guess = props.guess;
+        
         return (
             <Form>                                                                                                                                                                       
-                <Form.Control bsPrefix="" type="text" value="" placeholder="" readOnly />                                                                                                                                                                           
-                <Form.Control bsPrefix="" type="text" value="" placeholder="" readOnly />                                                                                                                                                                           
-                <Form.Control bsPrefix="" type="text" value="" placeholder="" readOnly />                                                                                                                                                                           
+                <Form.Control bsPrefix="" type="text" value={!guess ? "" : guess[0]} placeholder="" readOnly />                                                                                                                                                                           
+                <Form.Control bsPrefix="" type="text" value={!guess ? "" : guess[1]}  placeholder="" readOnly />                                                                                                                                                                           
+                <Form.Control bsPrefix="" type="text" value={!guess ? "" : guess[2]}  placeholder="" readOnly />                                                                                                                                                                           
                 <Form.Select bsPrefix="" aria-label="Default select example" readOnly>                    
                         <option value="thousand">Thousand</option>                                                                                                                           
                         <option value="million">Million</option>                                                                                                                             
