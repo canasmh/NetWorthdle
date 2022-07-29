6e912870fab1e375d117959168a64d7067cd6003
canasmh <canasmh@yahoo.com>
2022-07-28 21:28:41 -0400

    Create and style the Header. Need to add some font-awesome icons

---
+++ client/src/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+    return (
+        <div className="header">
+            <span className="left">Inf</span>
+            <h1>
+                <span className="green">Net</span><span className="gold">Worth</span>dle</h1><span className="right">Stats</span>
+        </div>
+    )
+}
+
+export default Header
