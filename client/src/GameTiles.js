b43bd13d602c2cbdbf89ac1fb5af5e3af4eb6479
canasmh <canasmh@yahoo.com>
2022-07-29 18:34:55 -0400

    Set the unit when value changes

--- client/src/GameTiles.js
+++ client/src/GameTiles.js
@@ -6,13 +6,19 @@ import "./GameTiles.css"
 function GameTiles(props) {
 
         const guess = props.guess;
-        
+
+        function changeUnit(event) {
+            const newMoneyUnit = event.target.value;
+
+            props.setMoneyUnit(newMoneyUnit)
+        }
+
         return (
             <Form>                                                                                                                                                                       
                 <Form.Control bsPrefix="" type="text" value={!guess ? "" : guess[0]} placeholder="" readOnly />                                                                                                                                                                           
-                <Form.Control bsPrefix="" type="text" value={!guess ? "" : guess[1]}  placeholder="" readOnly />                                                                                                                                                                           
-                <Form.Control bsPrefix="" type="text" value={!guess ? "" : guess[2]}  placeholder="" readOnly />                                                                                                                                                                           
-                <Form.Select bsPrefix="" aria-label="Default select example" readOnly>                    
+                <Form.Control bsPrefix="" type="text" value={!guess ? "" : guess[1]} placeholder="" readOnly />                                                                                                                                                                           
+                <Form.Control bsPrefix="" type="text" value={!guess ? "" : guess[2]} placeholder="" readOnly />                                                                                                                                                                           
+                <Form.Select bsPrefix="" aria-label="Default select example" onChange={changeUnit} readOnly>                    
                         <option value="thousand">Thousand</option>                                                                                                                           
                         <option value="million">Million</option>                                                                                                                             
                         <option value="billion">Billion</option>                                                                                                                                 
