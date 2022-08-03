673b9fca7a60d38a1cf49c773e58f472cd0e8152
canasmh <canasmh@yahoo.com>
2022-08-02 22:48:49 -0400

    Add the font-awesome icons for the header

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro'
 import "./Header.css";
+
 
 function Header() {
     return (
         <div className="header">
-            <span className="left">Inf</span>
+            <span className="left"><FontAwesomeIcon icon="fa-solid fa-circle-info" /></span>
             <h1>
-                <span className="green">Net</span><span className="gold">Worth</span>dle</h1><span className="right">Stats</span>
+                <span className="green">Net</span><span className="gold">W<FontAwesomeIcon icon="fa-solid fa-circle-dollar" />rth</span>dle</h1><span className="right"><FontAwesomeIcon icon="fa-solid fa-chart-simple" /></span>
         </div>
     )
 }
