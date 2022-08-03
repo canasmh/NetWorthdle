43f02989e31a0ec8263daeabe3fd8d54d3dd7eff
canasmh <canasmh@yahoo.com>
2022-08-02 23:24:16 -0400

    Removed unused imports

--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-// import { fas } from '@fortawesome/free-solid-svg-icons'
 import { fas } from '@fortawesome/pro-solid-svg-icons'
-import { faCircleDollar } from '@fortawesome/pro-regular-svg-icons'
-// import { faTwitter, faFontAwesome } from '@fortawesome/free-brands-svg-icons'
 
-library.add(fas)//, faCircleDollar) //, faTwitter, faFontAwesome)
+library.add(fas)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
