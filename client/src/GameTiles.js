ef04bb43c9e798188c985c913dca12fee429a270
canasmh <canasmh@yahoo.com>
2022-08-13 22:19:34 -0400

    Change the background color of disabled select. Also, on the second time of loading the page (after playing already), Need to manually input the 'unit' value. Or at least that was the simplest way I could think of doing it

--- client/src/GameTiles.js
+++ client/src/GameTiles.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
+import { todaysDate } from "./Dates";
 import "./GameTiles.css";
 
 
@@ -31,13 +32,14 @@ function GameTiles(props) {
 
         return (
             <Form>                                                                                                                                                                       
-                <Form.Control bsPrefix={classes[0]} type="text" value={!guess ? "" : guess[0]} placeholder="" readOnly disabled={props.disable} />                                                                                                                                                                           
-                <Form.Control bsPrefix={classes[1]} type="text" value={!guess ? "" : guess[1]} placeholder="" readOnly disabled={props.disable} />                                                                                                                                                                           
-                <Form.Control bsPrefix={classes[2]} type="text" value={!guess ? "" : guess[2]} placeholder="" readOnly disabled={props.disable} />                                                                                                                                                                           
-                <Form.Select bsPrefix={classes[3]} aria-label="Default select example" onChange={changeUnit} readOnly disabled={props.disable}>                    
-                        <option value="thousand">Thousand</option>                                                                                                                           
-                        <option value="million">Million</option>                                                                                                                             
-                        <option value="billion">Billion</option>                                                                                                                                 
+                <Form.Control bsPrefix={classes[0]} type="text" value={!guess ? "" : guess[0]} placeholder="" readOnly disabled={props.disable} />                                                                                                                                                              
+                <Form.Control bsPrefix={classes[1]} type="text" value={!guess ? "" : guess[1]} placeholder="" readOnly disabled={props.disable} />  
+                <Form.Control bsPrefix={classes[2]} type="text" value={!guess ? "" : guess[2]} placeholder="" readOnly disabled={props.disable} />
+                <Form.Select bsPrefix={classes[3]} aria-label="Default select example" onChange={changeUnit} readOnly disabled={props.disable}>
+                    {localStorage.lastPlayed === todaysDate()  && <option>{props.guess ? props.guess.slice(4, 5).toUpperCase() + props.guess.slice(5) : ""}</option>}
+                    <option value="thousand">Thousand</option>                                                                                                                           
+                    <option value="million">Million</option>                                                                                                                             
+                    <option value="billion">Billion</option>                                                                                                                                 
                 </Form.Select>                                                                                                                                                           
             </Form>)
     }
