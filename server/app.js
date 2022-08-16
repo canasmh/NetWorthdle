fbe98fa6919d93c41312362dfe5d00445ba510fd
canasmh <canasmh@yahoo.com>
2022-08-15 20:22:51 -0400

    Install and require mongoose

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 3001
+const mongoose = require('mongoose');
+const mongoURI = 'mongodb://localhost:27017/Celebrities'
+
 const app = express();
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
