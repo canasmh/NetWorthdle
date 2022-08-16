7fe9fc5a388a5652da9d68c6e967321a12599206
canasmh <canasmh@yahoo.com>
2022-08-15 20:39:24 -0400

    Create the collections, celebs, which stores the celeb data, and playedCelebs, which tracks the day each celeb was played. Also wrote functions to convert dates, same as in client

--- server/app.js
+++ server/app.js
@@ -1,11 +1,93 @@
 const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3001;
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb://localhost:27017/Celebrities'
+const mongoURI = 'mongodb://localhost:27017/Celebrities';
 
 const app = express();
 app.use(express.static(path.resolve(__dirname, '../client/build')));
+mongoose.connect(mongoURI);
+
+// Functions to convert Date
+function convertDate(dateObj) {
+    const dd = String(dateObj.getDate()).padStart(2, '0')
+    const yyyy = String(dateObj.getFullYear())
+    var mm = ''
+
+    switch (dateObj.getMonth().toString()) {
+        case '0':
+            mm = "January";
+            break;
+        case '1':
+            mm = "February";
+            break;
+        case '2':
+            mm = "March";
+            break;
+        case '3':
+            mm = "April";
+            break;
+        case '4':
+            mm = "May";
+            break;
+        case '5':
+            mm = "June";
+            break;
+        case '6':
+            mm = "July";
+            break;
+        case '7':
+            mm = "August";
+            break;
+        case '8':
+            mm = "September";
+            break;
+        case '9':
+            mm = "October";
+            break;
+        case '10':
+            mm = "November";
+            break;
+        case '11':
+            mm = "December";
+            break;
+        default:
+            mm = null;
+            console.log(`Month ${dateObj.getMonth().toString()} not accounted for`);
+    }
+
+    return mm + " " + dd + ", " + yyyy
+}
+
+function todaysDate() {
+    const today = new Date()
+    return convertDate(today)
+}
+
+function yesterdaysDate() {
+    const today = new Date()
+    const yesterday = new Date(today)
+
+    yesterday.setDate(yesterday.getDate() - 1)
+    
+    return convertDate(yesterday)
+}
+
+const celebSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    net_worth: {type: String, required: true},
+    nationality: {type: String},
+    occupation: {type: Array},
+    birthday: {type: String},
+});
+
+const celebPlayedSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    date: {type: String, default: todaysDate()}
+});
+
+const Celeb = new mongoose.model("celeb", celebSchema);
+const celebPlayed = new mongoose.model("PlayedCeleb", celebPlayedSchema);
 
 app.get('*', function (req, res) {
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
