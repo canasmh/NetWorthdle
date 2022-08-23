a33cd5023311c480cc6f2079548bae04f5b1511b
canasmh <canasmh@yahoo.com>
2022-08-22 22:55:59 -0400

    Create separate functions for the enter and delete buttons. This should fix the bug that prevents users from submitting their guess when they click directly on the font-awesome icons

--- client/src/Keyboard.js
+++ client/src/Keyboard.js
@@ -109,43 +109,37 @@ function Keyboard(props) {
     }
 
     function updateCurrentGuess(event) {
-        const id = event.target.id
-        const parentId = event.target.parentElement.id
-        const currentGuess = props.currentGuess
-        const unit = props.moneyUnit
-        const enterButton = <path fill="currentColor" d="M137.4 438.6l-128-128c-12.5-12.5-12.5-32.75 0-45.25l128-127.1c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L109.3 256H432C440.8 256 448 248.8 448 240V64c0-17.67 14.31-32 32-32s32 14.33 32 32v176c0 44.11-35.88 80-80 80H109.3l73.38 73.37C188.9 399.6 192 407.8 192 415.1s-3.125 16.38-9.375 22.62C170.1 451.1 149.9 451.1 137.4 438.6z"></path>
-        const deleteButton = <path fill="currentColor" d="M0 128C0 92.65 28.65 64 64 64H370.7C387.7 64 403.1 70.74 416 82.75L566.6 233.4C572.6 239.4 576 247.5 576 256C576 264.5 572.6 272.6 566.6 278.6L416 429.3C403.1 441.3 387.7 448 370.7 448H64C28.65 448 0 419.3 0 384V128zM143 208.1L190.1 255.1L143 303C133.7 312.4 133.7 327.6 143 336.1C152.4 346.3 167.6 346.3 176.1 336.1L223.1 289.9L271 336.1C280.4 346.3 295.6 346.3 304.1 336.1C314.3 327.6 314.3 312.4 304.1 303L257.9 255.1L304.1 208.1C314.3 199.6 314.3 184.4 304.1 175C295.6 165.7 280.4 165.7 271 175L223.1 222.1L176.1 175C167.6 165.7 152.4 165.7 143 175C133.7 184.4 133.7 199.6 143 208.1V208.1z"></path>
+        const id = event.target.id;
+        const currentGuess = props.currentGuess;
 
-        if (id === "backspace" || parentId === "backspace" || event.target === deleteButton) {
-            if (currentGuess.length > 0) {
-                props.setCurrentGuess(currentGuess.slice(0, currentGuess.length - 1))
-            }
+        if (currentGuess.length < 3) {
+            props.setCurrentGuess(oldGuess => oldGuess + id[1]);
+        }
+    }
 
-        } else if (id === "enter" || parentId === "enter" || event.target === enterButton) {
-            if (currentGuess.length < 3) {
-                console.log("Not enough Entries")
-            } else {
-                // Check the guess
-                checkGuess(currentGuess + " " + unit)
+    function deleteGuessEntry() {
+        const currentGuess = props.currentGuess;
+        if (currentGuess.length > 0) {
+            props.setCurrentGuess(currentGuess.slice(0, currentGuess.length - 1));
+        }
+    }
 
-                // Add Guess to Guess list
-                props.setGuesses(guesses => [...guesses, currentGuess + " " + unit])
+    function enterGuess() {
+        const currentGuess = props.currentGuess;
+        const unit = props.moneyUnit;
 
-                // Reset Guess
-                props.setCurrentGuess("")
-                props.setMoneyUnit("thousand")
-            }
-        } else if (id === "") {
-            console.log(`Unaccounted ID: ${id}\nParent ID: ${parentId}`)
-            console.log(event.target)
-            console.log(typeof event.target)
-            console.log(<FontAwesomeIcon icon="fa-solid fa-arrow-turn-down-left" />)
-
+        if (currentGuess.length < 3) {
+            console.log("Not enough Entries");
         } else {
-            if (currentGuess.length < 3) {
-                props.setCurrentGuess(oldGuess => oldGuess + id[1])
-            }
+            // Check the guess
+            checkGuess(currentGuess + " " + unit);
 
+            // Add Guess to Guess list
+            props.setGuesses(guesses => [...guesses, currentGuess + " " + unit]);
+
+            // Reset Guess
+            props.setCurrentGuess("");
+            props.setMoneyUnit("thousand");
         }
 
     }
@@ -168,9 +162,9 @@ function Keyboard(props) {
                 <button className={"keyboard-button " + keyClass[9]} id="n9" onClick={!gamePlayed ? updateCurrentGuess : null}>9</button>                                                                                                  
             </div>                                                                                                                                                                                          
             <div className="keyboard-row">                                                                                                                                                                     
-                <button className="keyboard-button" id="backspace" onClick={!gamePlayed ? updateCurrentGuess : null}><FontAwesomeIcon icon="fa-solid fa-delete-right" /></button>                                                                  
+                <button className="keyboard-button" id="backspace" onClick={!gamePlayed ? deleteGuessEntry : null}><FontAwesomeIcon icon="fa-solid fa-delete-right" /></button>                                                                  
                 <button className={"keyboard-button " + keyClass[0]} id="n0" onClick={!gamePlayed ? updateCurrentGuess : null}>0</button>                                                                                                  
-                <button className="keyboard-button" id="enter" onClick={!gamePlayed ? updateCurrentGuess : null}><FontAwesomeIcon icon="fa-solid fa-arrow-turn-down-left" /></button>                                                                   
+                <button className="keyboard-button" id="enter" onClick={!gamePlayed ? enterGuess : null}><FontAwesomeIcon icon="fa-solid fa-arrow-turn-down-left" /></button>                                                                   
             </div>                                                                                                                                                                                          
         </div>
     )
