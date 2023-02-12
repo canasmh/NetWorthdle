3086b1fc696262a48555009aceaa9fd942b05f6d
canasmh <canasmh@yahoo.com>
2023-02-11 22:18:04 -0500

    Change non-semantic 'div' tags to 'section' tags

--- client/src/GameBoard.js
+++ client/src/GameBoard.js
@@ -20,12 +20,12 @@ function GameBoard(props) {
     const gameOver = localStorage.lastPlayed === todaysDate();
 
     return (
-        <div className="game-board">
+        <section className="game-board">
             <GameTiles guess={guesses.length > 0 ? guesses[0] : null} currentGuess={guesses.length === 0 ? currentGuess : null} setMoneyUnit={props.setMoneyUnit} disable={guesses.length > 0 || gameOver ? true : false} classes={props.tileClasses.firstRow} />
             <GameTiles guess={guesses.length > 1 ? guesses[1] : null} currentGuess={guesses.length === 1 ? currentGuess : null} setMoneyUnit={props.setMoneyUnit} disable={guesses.length > 1 || gameOver ? true : false} classes={props.tileClasses.secondRow} />
             <GameTiles guess={guesses.length > 2 ? guesses[2] : null} currentGuess={guesses.length === 2 ? currentGuess : null} setMoneyUnit={props.setMoneyUnit} disable={guesses.length > 2 || gameOver ? true : false} classes={props.tileClasses.thirdRow} />
             <GameTiles guess={guesses.length > 3 ? guesses[3] : null} currentGuess={guesses.length === 3 ? currentGuess : null} setMoneyUnit={props.setMoneyUnit} disable={guesses.length > 3 || gameOver ? true : false} classes={props.tileClasses.fourthRow} />
-        </div>
+        </section>
     )
 }
 
