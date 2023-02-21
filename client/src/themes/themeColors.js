030943ba6486980723a144c65c5c8ddcf8586b29
canasmh <canasmh@yahoo.com>
2023-02-21 18:37:18 -0500

    Create themes array containg the colors used in each theme as well as the theme name

---
+++ client/src/themes/themeColors.js
@@ -0,0 +1,58 @@
+const themes = [
+    {
+        id: 0,
+        name: "original",
+        primary: '#355764',
+        secondary: '#81CACF',
+        tertiary: '#FFFFFF',
+        correct: '#5A8F7B',
+        correctText: '#D3D3D3',
+        present: '#FFEA11',
+        presentText: '#A9A9A9',
+        absent: '#373737',
+        absentText: '#D3D3D3'
+
+    },
+    {
+        id: 1,
+        name: "dark",
+        primary: '#111111',
+        secondary: '#AAAAAA',
+        tertiary: '#EEEEEE',
+        correct: '#5A8F7B',
+        correctText: '#D3D3D3',
+        present: '#FFEA11',
+        presentText: '#A9A9A9',
+        absent: '#373737',
+        absentText: '#D3D3D3'
+    },
+    {
+        id: 2,
+        name: "light",
+        primary: '#EEEEEE',
+        secondary: '#666666',
+        tertiary: '#333333',
+        correct: '#5A8F7B',
+        correctText: '#D3D3D3',
+        present: '#E1CD07',
+        presentText: '#E9E9E9',
+        absent: '#666666',
+        absentText: '#FFFFFF'
+    },
+    {
+        id: 3,
+        name: "highContrast",
+        primary: '#000000',
+        secondary: '#F2F54F',
+        tertiary: '#F2F54F',
+        correct: '#F200E8',
+        correctText: '#000000',
+        present: '#32FAEE',
+        presentText: '#000000',
+        absent: '#AAAAAA',
+        absentText: '#000000'
+
+    }
+]
+
+export default themes;
