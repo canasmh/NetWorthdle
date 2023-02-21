16a96028bccde29185b46686f1e67ccd2eee96ba
canasmh <canasmh@yahoo.com>
2023-02-20 22:17:52 -0500

    Add a color palette icon that will be used for users to select a theme

--- client/src/Header.js
+++ client/src/Header.js
@@ -8,7 +8,7 @@ function Header(props) {
         <header className="header">
             
             
-            <span className="left"><FontAwesomeIcon icon="fa-solid fa-circle-info" onClick={() => props.showHelp(true)}/></span>
+            <span className="left"><FontAwesomeIcon icon="fa-solid fa-circle-info" onClick={() => props.showHelp(true)}/><FontAwesomeIcon icon="fa-solid fa-palette" /></span>
             <h1>
                 <span className="green">Net</span><span className="gold">W<FontAwesomeIcon icon="fa-solid fa-circle-dollar" />rth</span>dle
             </h1>
