a4be64a14a0b72a1f08878107e0f25c578b9f819
canasmh <canasmh@yahoo.com>
2023-03-09 18:40:35 -0500

    Implement redux and the custom theme

--- client/src/components/modals/GameHelp.js
+++ client/src/components/modals/GameHelp.js
@@ -1,14 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import "./GameModals.css";
+import { useSelector } from "react-redux";
 
 function GameHelp(props) {
+    const theme = useSelector(state => state.theme.theme);
+    const [hover, setHover] = useState(false);
+
+    const handleMouseEnter = () => {
+      setHover(true);
+    }
+
+    const handleMouseExit = () => {
+      setHover(false);
+    }
+
+    const handleClick = () => {
+      props.onHide();
+      setHover(false);
+    }
+    const correct = {
+      backgroundColor: theme.correct,
+      display: 'inline-block',
+      width: '6rem',
+      height: '1.5rem'
+    }
+
+    const present = {
+      backgroundColor: theme.present,
+      display: 'inline-block',
+      width: '6rem',
+      height: '1.5rem'
+    }
+
+    const absent = {
+      backgroundColor: theme.absent,
+      display: 'inline-block',
+      width: '6rem',
+      height: '1.5rem'
+    }
+
+    const buttonOutline = {
+      backgroundColor: !hover ? theme.primary : theme.tertiary,
+      color: hover ? theme.primary : theme.tertiary,
+      border: `1px solid ${hover ? theme.primary : theme.tertiary}`,
+    }
+
     return (
       <Modal
         {...props}
         size="md"
         aria-labelledby="contained-modal-title-vcenter"
+        contentClassName={`content-${theme.name}`}
         centered
       >
         <Modal.Header>
@@ -21,17 +65,23 @@ function GameHelp(props) {
             <span className="centered">Enter a 3 digit number (from <strong>001</strong> to <strong>999</strong>) and an amount (i.e., <strong>Million</strong>).</span>
         </p>
         <p>
-            <span className="green-bg">GREEN</span> means that the number and/or amount is in the correct spot
+            <span style={correct}>&nbsp;</span> means that the number and/or amount is in the correct spot
         </p>
         <p>
-            <span className="yellow-bg">YELLOW</span> means that that number <em>is</em> present, but is not in the correct spot.
+            <span style={present}>&nbsp;</span> means that that number <em>is</em> present, but is not in the correct spot.
         </p>
         <p>
-            <span className="gray-bg">GRAY</span> means that the number and/or amount is not present.
+            <span style={absent}>&nbsp;</span> means that the number and/or amount is not present.
         </p>
         </Modal.Body>
         <Modal.Footer>
-          <Button onClick={props.onHide} variant="outline-light">Close</Button>
+          <Button 
+            onClick={handleClick} 
+            style={buttonOutline} 
+            onMouseEnter={handleMouseEnter} 
+            onMouseLeave={handleMouseExit}>
+              Close
+          </Button>
         </Modal.Footer>
       </Modal>
     );
